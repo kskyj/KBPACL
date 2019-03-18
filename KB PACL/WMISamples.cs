3139b38c417a525bd15cd7df66957805a5b35bc4
SungJinKim <kskyj@live.co.kr>
2019-03-19 01:14:43 +0900

    c# 기본 printer 라이브러리를 활용예제 추가

--- KB PACL/WMISamples.cs
+++ KB PACL/WMISamples.cs
@@ -3,6 +3,7 @@ using System.Management;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Printing;
 using Microsoft.Win32;
 
 
@@ -26,9 +27,12 @@ namespace KB_PACL
 
         private static void WMICount()
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT PortName FROM Win32_Printer");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Printer");
             Console.WriteLine(searcher.Get().Count);
-            Console.WriteLine(searcher.Get());
+            ManagementObjectSearcher searcher2 = new ManagementObjectSearcher("root\\CIMV2", "SELECT PortName FROM Win32_Printer");
+            Console.WriteLine(searcher2.Get().Count);
+            ManagementObjectSearcher searcher3 = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Printer WHERE PortName ='test1'");
+            Console.WriteLine(searcher3.Get().Count);
         }
 
         private static void WMISample1()
@@ -46,15 +50,55 @@ namespace KB_PACL
             }
             //Console.WriteLine(res);
         }
-
-        static void Main(string[] args)
+        private static void WMISample2()
         {
-            Console.WriteLine("getRegisty----------");
-            getRegistrySample();
-            Console.WriteLine("WMICount----------");
-            WMICount();
-            Console.WriteLine("WMISample1----------");
-            WMISample1();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_TCPIPPrinterPort");
+            String[] result = new String[searcher.Get().Count];
+            int i = 0;
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                result[i] = queryObj["Name"].ToString();
+                Console.WriteLine(result[i]);
+                i++;
+            }
+            //Console.WriteLine(res);
+        }
+        private static void CsharpAPI()
+        {
+            var ps = new PrintServer();
+            var queues = ps.GetPrintQueues();
+            var pq = queues.Where(t => t.FullName.Contains("Epson")).First();
+            if (pq == null) { 
+                return;
+            }
+            Console.WriteLine(pq);
+            pq.Refresh();
+            var jobs = pq.GetPrintJobInfoCollection();
+            Console.WriteLine(jobs);
+            foreach (var job in jobs)
+            {
+                job.Cancel();
+            }
+        }
+    static void Main(string[] args)
+        {
+            while (true)
+           {
+                Console.WriteLine("getRegisty----------");
+                getRegistrySample();
+                Console.WriteLine("WMICount----------");
+                WMICount();
+                Console.WriteLine("WMISample1----------");
+                WMISample1();
+                Console.WriteLine("WMISample2----------");
+                WMISample2();
+
+                //WIN32 말고 C# API를 이용하자.
+                CsharpAPI();
+
+                // 마지막에 항상 추가, 빌드방법 Windows Application 으로 변경
+                GC.Collect();
+            }
         }
 
         
